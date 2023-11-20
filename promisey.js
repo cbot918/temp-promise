054aca13b4dc3c0d761b55427c0af2dc4dc30b75
cbot918 <cbot918@gmail.com>
2023-11-20 15:57:17 +0800

    init

---
+++ promisey.js
@@ -0,0 +1,71 @@
+const PENDING = "PENDING";
+const FULFILLED = "FULFILLED";
+const REJECTED = "REJECTED";
+
+function promisey(executor) {
+  let status = PENDING;
+  let value = undefined;
+  let reason = undefined;
+
+  let onResolvedCallbacks = [];
+  let onRejectedCallbacks = [];
+
+  function resolve(_value) {
+    if (status === PENDING) {
+      status = FULFILLED;
+      value = _value;
+    }
+
+    onResolvedCallbacks.forEach((fn) => fn());
+  }
+
+  function reject(_reason) {
+    if (status === PENDING) {
+      status = REJECTED;
+      reason = _reason;
+    }
+
+    onRejectedCallbacks.forEach((fn) => fn());
+  }
+
+  try {
+    executor(resolve, reject);
+  } catch (error) {
+    reject(error);
+  }
+
+  function then(onFulfilled, onRejected) {
+    if (status === FULFILLED) {
+      onFulfilled(value);
+    }
+    if (status === REJECTED) {
+      onRejected(reason);
+    }
+
+    if (status === PENDING) {
+      onResolvedCallbacks.push(() => {
+        onFulfilled(value);
+      });
+      onRejectedCallbacks.push(() => {
+        onRejected(reason);
+      });
+    }
+  }
+
+  return {
+    then,
+  };
+}
+
+const promise = promisey((resolve, reject) => {
+  setTimeout(() => {
+    resolve("成功");
+  }, 2000);
+}).then(
+  (data) => {
+    console.log("success", data);
+  },
+  (err) => {
+    console.log("faild", err);
+  }
+);
